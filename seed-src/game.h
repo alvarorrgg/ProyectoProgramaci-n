f138e7df5bee3781516749f0e96c3a788e9e0811
alvarorrgg <alvaro.rodriguezr02@estudiante.uam.es>
2021-02-15 11:34:54 +0100

    Archivos game sin creación de espacios [NO LLEVA COMENTARIOS]

--- seed-src/game.h
+++ seed-src/game.h
@@ -11,16 +11,8 @@
 
 #ifndef GAME_H
 #define GAME_H
+#include "game_reader.h"
 
-#include "command.h"
-#include "space.h"
-
-typedef struct _Game{
-  Id player_location;
-  Id object_location;
-  Space* spaces[MAX_SPACES + 1];
-  T_Command last_cmd;
-} Game;
 
 STATUS game_create(Game* game);
 STATUS game_create_from_file(Game* game, char* filename);
@@ -28,7 +20,7 @@ STATUS game_update(Game* game, T_Command cmd);
 STATUS game_destroy(Game* game);
 BOOL   game_is_over(Game* game);
 void   game_print_data(Game* game);
-Space* game_get_space(Game* game, Id id);
+
 Id     game_get_player_location(Game* game);
 Id     game_get_object_location(Game* game);
 T_Command game_get_last_command(Game* game);
