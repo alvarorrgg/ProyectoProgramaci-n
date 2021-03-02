f759c724aa89b731247e4d10c48ffbf44aee60a6
Albertovg <alberto.vicente@estudiante.uam.es>
2021-03-02 19:12:57 +0100

    Hola

---
+++ seed-src/die.c
@@ -0,0 +1,21 @@
+/** 
+ * @brief It tests set module
+ * 
+ * @file die.c
+ * @author R2
+ * @version 2.0 
+ * @date 02-03-2021
+ * @updated 02-03-2021
+ * @copyright GNU Public License
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "die.h"
+
+struct Die{
+
+
+
+};
---
+++ seed-src/die.h
@@ -0,0 +1,17 @@
+/** 
+ * @brief Archivo cabecera del modulo set.h, en el se define la estructura de datos Object y sus funciones.
+ * 
+ * @file die.h
+ * @author R2
+ * @version 1.0 
+ * @date 02-03-2021 
+ * @copyright GNU Public License
+ */
+ 
+#ifndef DIE_H
+#define DIE_H
+#include "types.h"
+#include "set.h"
+#define MAX_IDS 100
+
+typedef struct _Die Die;
